33a0d9cc91571a215aad3dffd6021a303ec58f8b
Michael Leonffu <leonffu.michael@gmail.com>
2018-01-30 19:31:39 -0800

    Removed some extra stuff in server

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-var 	express 		= require('express')
-var 	app				= express()
-const	path 			= require('path')
-var		port 			= process.env.PORT || 3000
+const 	app 			= require('express')()
+const	port 			= process.env.PORT || 3000
 
-var 	morgan 			= require('morgan')
-var 	bodyParser 		= require('body-parser')
+const 	morgan 			= require('morgan')
+const 	bodyParser 		= require('body-parser')
 
-var 	MongoClient		= require('mongodb').MongoClient
-var 	configDB 		= require('./config/database.js')
+const 	MongoClient		= require('mongodb').MongoClient
+const 	configDB 		= require('./config/database.js')
+const 	assert			= require('assert')
 
 app.use(morgan('dev')) // log every request to the console
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }))
 
 MongoClient.connect(configDB.url, function(err,client){
-	if(err){
-		console.log(err)
-		return
-	}
+	assert.equal(null, err)
 	require('./api/api')(app, client.db('JuicyData'))
 })
 
